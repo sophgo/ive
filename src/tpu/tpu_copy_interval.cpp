2a0ffcf54b1921794d684f5dbaeffa41cc15aaa8
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-02-11 17:01:19 +0800

    [copy] Fix interval copy result
    Symptom: Vertical interval disappeared.
    Root cause: stride h is not set.
    Solution: Set before kernel operation.
    Note: NA
    
    Change-Id: I721d2cd8dd9b469521ddee99a1a7de2b7a82654a

--- src/tpu/tpu_copy_interval.cpp
+++ src/tpu/tpu_copy_interval.cpp
@@ -17,8 +17,10 @@ int IveTPUCopyInterval::SliceSetup(SliceRes &slice_res, SliceRes *tg_in_res, Sli
   *tg_out_res = slice_res;
   tg_out_res->h.skip *= m_verti;
   tg_out_res->w.skip *= m_hori;
+  tg_out_res->h.left *= m_verti;
   tg_out_res->h.slice *= m_verti;
   tg_out_res->w.slice *= m_hori;
+  tg_out_res->w.left *= m_hori;
   return BM_SUCCESS;
 }
 
@@ -38,9 +40,6 @@ int IveTPUCopyInterval::runSetup(bmctx_t *ctx, bmk1880v2_context_t *bk_ctx,
   tl_shape2.h = tg_out_slices[0].h;
   tl_shape2.w = tg_out_slices[0].w;
   auto *tl_output = allocTLMem(bk_ctx, tl_shape2, FMT_U8, 1);
-  // We only have to change the w stride cause the m_hori skip is caluclated
-  // already in h stride using tl_shape2.
-  tl_output->stride.w *= m_hori;
   m_p_copy.bf16_enable = tl_input->fmt == FMT_BF16 ? 1 : 0;
   m_p_copy.src = tl_input;
   m_p_copy.dst = tl_output;
@@ -48,7 +47,6 @@ int IveTPUCopyInterval::runSetup(bmctx_t *ctx, bmk1880v2_context_t *bk_ctx,
   bmk1880v2_tdma_tg2l_tensor_fill_constant_param_t p_fill;
   p_fill.constant = 0;
   p_fill.dst = tl_output;
-
   bmk1880v2_tdma_tg2l_bf16_tensor_fill_constant(bk_ctx, &p_fill);
   bmruntime_bmkernel_submit(*ctx);
 
@@ -61,7 +59,13 @@ void IveTPUCopyInterval::operation(bmctx_t *ctx, bmk1880v2_context_t *bk_ctx) {
   auto *tl_in = m_tl_vec[0];
   auto *tl_out = m_tl_vec[1];
   auto shape = tl_out->shape;
+  auto stride = tl_out->stride;
+  // Change shape and stride for interval hack.
   tl_out->shape = tl_in->shape;
+  tl_out->stride.w *= m_hori;
+  tl_out->stride.h *= m_verti;
   bmk1880v2_tiu_bf16_element_wise_copy(bk_ctx, &m_p_copy);
+  // Change to default value.
   tl_out->shape = shape;
+  tl_out->stride = stride;
 }
