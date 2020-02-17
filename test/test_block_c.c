d85181c8d84cb045508712c655267f6cbbfcb7bb
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-02-17 16:12:14 +0800

    [test][block] TPU uses round when converting bf16 to uchar
    Symptom: NA
    Root cause: NA
    Solution: NA
    Note: Remove magic number 5 (CELL_SZ)
    
    Change-Id: Id2e456d931c481b040ba5c743990ac1548eef73b

--- test/test_block_c.c
+++ test/test_block_c.c
@@ -1,5 +1,6 @@
 #include "ive.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,7 +34,8 @@ static char test_array[] = {
 #define TEST_H 20
 #define CELL_SZ 5
 
-int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size, IVE_SRC_IMAGE_S *src, IVE_DST_IMAGE_S *dst_u8, IVE_DST_IMAGE_S *dst_fp32);
+int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size,
+            IVE_SRC_IMAGE_S *src, IVE_DST_IMAGE_S *dst_u8, IVE_DST_IMAGE_S *dst_fp32);
 
 int main(int argc, char **argv) {
   CVI_SYS_LOGGING(argv[0]);
@@ -75,14 +77,15 @@ int main(int argc, char **argv) {
   return ret;
 }
 
-int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size, IVE_SRC_IMAGE_S *src, IVE_DST_IMAGE_S *dst_u8, IVE_DST_IMAGE_S *dst_fp32) {
+int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size,
+            IVE_SRC_IMAGE_S *src, IVE_DST_IMAGE_S *dst_u8, IVE_DST_IMAGE_S *dst_fp32) {
   int ret = CVI_SUCCESS;
   float *cpu_result = malloc(res_h * res_w * sizeof(float));
   memset(cpu_result, 0, res_h * res_w * sizeof(float));
   for (size_t i = 0; i < TEST_H; i++) {
     for (size_t j = 0; j < TEST_W; j++) {
       char val = src->pu8VirAddr[0][i * TEST_W + j];
-      cpu_result[(int)(i / 5) * res_w + (int)(j / 5)] += val;
+      cpu_result[(int)(i / CELL_SZ) * res_w + (int)(j / CELL_SZ)] += val;
     }
   }
   for (size_t i = 0; i < res_h; i++) {
@@ -90,15 +93,11 @@ int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size, IVE_SRC_IM
       cpu_result[i * res_w + j] /= CELL_SZ * CELL_SZ * bin_size;
     }
   }
-
+  printf("U8 check:\n");
   for (size_t i = 0; i < res_h; i++) {
     for (size_t j = 0; j < res_w; j++) {
       float f_res = cpu_result[i * res_w + j];
-      int int_result = f_res;
-      // FIXME: Need check
-      if ((int)int_result != f_res) {
-        int_result++;
-      }
+      int int_result = round(f_res);
       if (int_result != dst_u8->pu8VirAddr[0][i * res_w + j]) {
         printf("%d %d \n", int_result, dst_u8->pu8VirAddr[0][i * res_w + j]);
         ret = CVI_FAILURE;
@@ -106,10 +105,12 @@ int cpu_ref(const int res_w, const int res_h, const CVI_U32 bin_size, IVE_SRC_IM
       }
     }
   }
+  printf("BF16 check:\n");
   for (size_t i = 0; i < res_h; i++) {
     for (size_t j = 0; j < res_w; j++) {
       if (cpu_result[i * res_w + j] != ((float*)dst_fp32->pu8VirAddr[0])[i * res_w + j]) {
-        printf("%f %f \n", cpu_result[i * res_w + j], ((float*)dst_fp32->pu8VirAddr[0])[i * res_w + j]);
+        printf("%f %f \n", cpu_result[i * res_w + j],
+                           ((float*)dst_fp32->pu8VirAddr[0])[i * res_w + j]);
         ret = CVI_FAILURE;
         break;
       }
