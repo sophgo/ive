811859605a31aeb5cb78a2a212adbf6e3df4ca2f
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-03-17 09:48:24 +0000

    [ive] Fix image VAddr offset generated from CVI_IVE_SubImage
    Symptom: Wrong VAddr offset
    Root cause: Should use src instead of dst height.
    Solution: As root cause.
    Note: NA
    
    Change-Id: Ica7340ea76bcd97a23bd7e7a581dbd816e115848

--- src/ive.cpp
+++ src/ive.cpp
@@ -216,7 +216,7 @@ CVI_S32 CVI_IVE_SubImage(IVE_HANDLE pIveHandle, IVE_SRC_IMAGE_S *pstSrc, IVE_DST
   pstDst->u16Height = cpp_img->m_tg.shape.h;
   pstDst->u16Reserved = pstSrc->u16Reserved;
 
-  int img_sz = cpp_img->m_tg.stride.h * pstDst->u16Height * pstSrc->u16Reserved;
+  int img_sz = cpp_img->m_tg.stride.h * pstSrc->u16Height * pstSrc->u16Reserved;
   for (size_t i = 0; i < cpp_img->m_tg.shape.c; i++) {
     pstDst->pu8VirAddr[i] = cpp_img->GetVAddr() + i * img_sz;
     pstDst->u64PhyAddr[i] = cpp_img->GetPAddr() + i * img_sz;
