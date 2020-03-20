164f57d71cd614d0a97e3558eb3350eab4062179
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-03-20 02:42:09 +0000

    [cmake][test] Remove deprecated cpp test cases
    Symptom: cpp test cases are deprecated.
    Root cause: IVE now use c test cases.
    Solution: Remove it.
    Note: NA
    
    Change-Id: I75efe169900c3b560ac8cf534f18b01866458347

--- clang-format.sh
+++ clang-format.sh
@@ -3,6 +3,4 @@ clang-format -i include/*.hpp
 clang-format -i include/tpu/*.hpp
 clang-format -i src/*.cpp
 clang-format -i src/tpu/*.cpp
-clang-format -i test/*.cpp
 clang-format -i test/*.c
-clang-format -i example/*.cpp
