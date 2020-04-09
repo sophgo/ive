e0d4d18b6e992413898a7fe0020f64fdbb4ee872
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-04-09 02:20:54 +0000

    [script] Refine sdk release script
    Symptom: Need to integrate with build project.
    Root cause: Release SDK.
    Solution: NA
    Note: NA
    
    Change-Id: I6aada0848b52599b85ee6bcbc222d2427570feb5

--- scripts/sdk_release.sh
+++ scripts/sdk_release.sh
@@ -2,23 +2,39 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IVE_ROOT=$(readlink -f $SCRIPT_DIR/../)
 TMP_WORKING_DIR=$IVE_ROOT/tmp
-IVE_SDK_NAME="ivesdk"-$(date '+%Y%m%d')
+# IVE_SDK_NAME="ivesdk"-$(date '+%Y%m%d')
 echo "Creating tmp working directory."
-mkdir -p $TMP_WORKING_DIR/build_sdk
-cd $TMP_WORKING_DIR/build_sdk
-cmake -G Ninja $IVE_ROOT -DBM_TARGET=soc \
-                         -DENABLE_SYSTRACE=OFF \
-                         -DCMAKE_BUILD_TYPE=SDKRelease \
-                         -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                         -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                         -DCMAKE_INSTALL_PREFIX=$IVE_SDK_INSTALL_PATH \
-                         -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                         -DCMAKE_TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-aarch64-linux.cmake
-make -j8 && make install
-cd $TMP_WORKING_DIR
-echo "Compressing SDK release..."
-tar cf - ivesdk -P | pv -s $(du -sb ivesdk | awk '{print $1}') | gzip > $IVE_ROOT/$IVE_SDK_NAME.tar.gz
-echo "Output md5 sum."
-md5sum $IVE_ROOT/$IVE_SDK_NAME.tar.gz
+if [[ "$1" == "cmodel" ]]; then
+    mkdir -p $TMP_WORKING_DIR/build_cmodel
+    pushd $TMP_WORKING_DIR/build_cmodel
+    cmake -G Ninja $IVE_ROOT -DENABLE_SYSTRACE=OFF \
+                             -DCMAKE_BUILD_TYPE=SDKRelease \
+                             -DMLIR_SDK_ROOT=$TPU_MLIR_INSTALL_PATH \
+                             -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
+                             -DCMAKE_INSTALL_PREFIX=$IVE_CMODEL_INSTALL_PATH \
+    ninja -j8 && ninja install
+    popd
+elif [[ "$1" == "soc" ]]; then
+    mkdir -p $TMP_WORKING_DIR/build_sdk
+    pushd $TMP_WORKING_DIR/build_sdk
+    cmake -G Ninja $IVE_ROOT -DBM_TARGET=soc \
+                            -DENABLE_SYSTRACE=OFF \
+                            -DCMAKE_BUILD_TYPE=SDKRelease \
+                            -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
+                            -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
+                            -DCMAKE_INSTALL_PREFIX=$IVE_SDK_INSTALL_PATH \
+                            -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
+                            -DCMAKE_TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-aarch64-linux.cmake
+    ninja -j8 && ninja install
+    popd
+else
+  echo "Unsupported build type."
+  exit 1
+fi
+# cd $TMP_WORKING_DIR
+# echo "Compressing SDK release..."
+# tar cf - ivesdk -P | pv -s $(du -sb ivesdk | awk '{print $1}') | gzip > $IVE_ROOT/$IVE_SDK_NAME.tar.gz
+# echo "Output md5 sum."
+# md5sum $IVE_ROOT/$IVE_SDK_NAME.tar.gz
 echo "Cleanup tmp folder."
 rm -r $TMP_WORKING_DIR
