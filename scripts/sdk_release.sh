182ffaafa6a7bb743528a53dd3eed4c067462798
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-04-13 03:21:46 +0000

    [cmake] Fix sdk_release.sh script
    Symptom: Build cmodel failed.
    Root cause: NA
    Solution: Changed CC, CXX to clang.
    Note: NA
    
    Change-Id: Ic4114ea14ee63cd0284a492458687876b5e64e50

--- scripts/sdk_release.sh
+++ scripts/sdk_release.sh
@@ -17,11 +17,12 @@ echo "Creating tmp working directory."
 if [[ "$1" == "cmodel" ]]; then
     mkdir -p $TMP_WORKING_DIR/build_cmodel
     pushd $TMP_WORKING_DIR/build_cmodel
+    CC=clang CXX=clang++ \
     cmake -G Ninja $IVE_ROOT -DENABLE_SYSTRACE=OFF \
-                             -DCMAKE_BUILD_TYPE=SDKRelease \
+                             -DCMAKE_BUILD_TYPE=Release \
                              -DMLIR_SDK_ROOT=$TPU_MLIR_INSTALL_PATH \
                              -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                             -DCMAKE_INSTALL_PREFIX=$IVE_CMODEL_INSTALL_PATH \
+                             -DCMAKE_INSTALL_PREFIX=$IVE_CMODEL_INSTALL_PATH
     ninja -j8 && ninja install
     popd
 elif [[ "$1" == "soc" ]]; then
