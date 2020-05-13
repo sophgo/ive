99fcf4cbba36a6648f3eef6ef5621e3d7dbdc74b
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-05-13 03:42:30 +0000

    [ive] Fix forget to get offset from CviImg offsets
    Symptom: May error if channel not originally 4096 aligned.
    Root cause: Forget to fix it.
    Solution: Use GetImgCOffsets()
    Note: NA
    
    Change-Id: I079a29276a9aff7d278635857997902905ba3b69

--- src/ive.cpp
+++ src/ive.cpp
@@ -324,9 +324,8 @@ CVI_S32 CVI_IVE_CreateImage2(IVE_HANDLE pIveHandle, IVE_IMAGE_S *pstImg, IVE_IMA
 
   size_t i_limit = cpp_img->GetImgChannel();
   for (size_t i = 0; i < i_limit; i++) {
-    int img_sz = cpp_img->GetImgStrides()[i] * cpp_img->GetImgHeights()[i] * fmt_size;
-    pstImg->pu8VirAddr[i] = cpp_img->GetVAddr() + i * img_sz;
-    pstImg->u64PhyAddr[i] = cpp_img->GetPAddr() + i * img_sz;
+    pstImg->pu8VirAddr[i] = cpp_img->GetVAddr() + cpp_img->GetImgCOffsets()[i];
+    pstImg->u64PhyAddr[i] = cpp_img->GetPAddr() + cpp_img->GetImgCOffsets()[i];
     pstImg->u16Stride[i] = cpp_img->GetImgStrides()[i];
   }
 
