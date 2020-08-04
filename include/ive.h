feddaeeab3e431a2b04d65ad823b3d5a22da356b
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-08-04 06:42:19 +0000

    [ive] Add CVI_IVE_Image2VideoFrame to header
    Symptom: Forget to move to header.
    Root cause: NA
    Solution: NA
    Note: NA
    
    Change-Id: I530f321bfb6ec1bc2e9bd2537540a6fd73ed3198

--- include/ive.h
+++ include/ive.h
@@ -106,6 +106,15 @@ CVI_S32 CVI_IVE_SubImage(IVE_HANDLE pIveHandle, IVE_SRC_IMAGE_S *pstSrc, IVE_DST
                          CVI_U16 u16X1, CVI_U16 u16Y1, CVI_U16 u16X2, CVI_U16 u16Y2);
 
 /**
+ * @brief Convert IVE_IMAGE_S to VIDEO_FRAME_S. The virtual address is available when converted.
+ *
+ * @param pstIISrc IVE_IMAGE_S input.
+ * @param pstVFDst VIDEO_FRAME_S output.
+ * @return CVI_S32 Return CVI_SUCCESS if operation succeeded.
+ */
+CVI_S32 CVI_IVE_Image2VideoFrame(IVE_IMAGE_S *pstIISrc, VIDEO_FRAME_S *pstVFDst);
+
+/**
  * @brief Convert VIDEO_FRAME_S to IVE_IMAGE_S. Note that this function does not map or unmap for
  *        you.
  *
