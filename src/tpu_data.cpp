aab7d5402e188e8745b58c4397b0397060d7f2b1
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-09-17 02:18:04 +0000

    [tpu_data] Temporarily bug fix when align to 4096
    Symptom: If input is bf16, output is u8 will failedin noKernel mode.
    Root cause: 4096 align cause channel size different.
    Solution: Temporarily multiply the fmt size after align.
    Note: NA
    
    Change-Id: I82932825a2725b6a28edbbc3977829065c3f0e75

--- src/tpu_data.cpp
+++ src/tpu_data.cpp
@@ -90,7 +90,10 @@ CviImg::CviImg(CVI_RT_HANDLE rt_handle, uint32_t img_h, uint32_t img_w,
   this->m_coffsets.push_back(this->m_size);
 #ifdef WORKAROUND_SCALAR_4096_ALIGN_BUG
   for (size_t i = 0; i < strides.size(); i++) {
-    this->m_size += Align64(strides[i] * heights[i] * getFmtSize(this->m_fmt), SCALAR_C_ALIGN);
+    printf("stride %u\n", strides[i]);
+    // FIXME: Should be Align64(strides[i] * heights[i] * getFmtSize(this->m_fmt), SCALAR_C_ALIGN)
+    // But there is a bug in core, so we use this method temporarily.
+    this->m_size += Align64(strides[i] * heights[i], SCALAR_C_ALIGN) * getFmtSize(this->m_fmt);
     if (strides[i] != strides[0]) {
       m_is_stride_ceq = false;
     }
@@ -113,7 +116,7 @@ CviImg::CviImg(CVI_RT_HANDLE rt_handle, uint32_t img_h, uint32_t img_w,
     }
   }
   AllocateDevice(rt_handle);
-
+  printf("size %lu\n", this->m_size);
 #ifdef WORKAROUND_SCALAR_4096_ALIGN_BUG
   uint64_t new_paddr = Align64(this->m_paddr, SCALAR_C_ALIGN);
   uint64_t offset = new_paddr - this->m_paddr;
