bbf8521f531ebdab6e13ad19edd1486dec0744cc
yangwen.huang <yangwen.huang@wisecore.com.tw>
2020-11-10 08:08:24 +0000

    [Bug][ive] Disable LOGD in SDKRelease build
    Symptom: syslog affects performance significantly.
    Root cause: Don't know.
    Solution: Disable syslog in SDKRelease build.
    Note: NA
    
    Change-Id: I32e2e12414f242a7e778ff9e33df4ccfd0b0a5ae

--- src/ive_log.hpp
+++ src/ive_log.hpp
@@ -4,7 +4,11 @@
 
 #define MODULE_NAME "ive"
 #define CVIAI_LOG_CHN LOG_LOCAL7
+#ifndef NDEBUG
 #define LOGD(fmt, ...) syslog(CVIAI_LOG_CHN | LOG_DEBUG, "[%s] " fmt, MODULE_NAME, ##__VA_ARGS__)
+#else
+#define LOGD(fmt, ...)
+#endif
 #define LOGI(fmt, ...) syslog(CVIAI_LOG_CHN | LOG_INFO, "[%s] " fmt, MODULE_NAME, ##__VA_ARGS__)
 #define LOGN(fmt, ...) syslog(CVIAI_LOG_CHN | LOG_NOTICE, "[%s] " fmt, MODULE_NAME, ##__VA_ARGS__)
 #define LOGW(fmt, ...) syslog(CVIAI_LOG_CHN | LOG_WARNING, "[%s] " fmt, MODULE_NAME, ##__VA_ARGS__)
