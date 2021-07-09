5beec694caf999cd8eda10c910df9a5c522f438a
Jeff Huang <jiaji.huang@cvitek.com>
2021-07-09 20:56:42 +0800

    [build] remove BUILD_TYPE args from build script
    Symptom: N/A
    Root cause: N/A
    Solution: N/A
    Note: N/A
    Change-Id: Ibc17aa44a8e268604b859d29b560cde3507e2b8b
    Reviewed-on: https://10.58.65.11:8087/c/ive/+/11387
    Reviewed-by: <jiaji.huang@cvitek.com>

--- scripts/sdk_release.sh
+++ scripts/sdk_release.sh
@@ -2,66 +2,36 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IVE_ROOT=$(readlink -f $SCRIPT_DIR/../)
 TMP_WORKING_DIR=$IVE_ROOT/tmp
-# Clone 3rdparty library systrace
-#pushd $IVE_ROOT/3rdparty
-#if [ ! -d "tracer" ]; then
-#  git clone ssh://git@10.34.33.3:8422/sys_app/tracer.git
-#fi
-#pushd tracer
-#git checkout origin/master
-#popd
-#popd
 
-# IVE_SDK_NAME="ivesdk"-$(date '+%Y%m%d')
 echo "Creating tmp working directory."
-if [[ "$1" == "cmodel" ]]; then
-    mkdir -p $TMP_WORKING_DIR/build_cmodel
-    pushd $TMP_WORKING_DIR/build_cmodel
-    CC=clang CXX=clang++ \
-    cmake -G Ninja $IVE_ROOT -DENABLE_SYSTRACE=OFF \
-                             -DCMAKE_BUILD_TYPE=SDKRelease \
-                             -DMLIR_SDK_ROOT=$TPU_MLIR_INSTALL_PATH \
-                             -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                             -DCMAKE_INSTALL_PREFIX=$IVE_CMODEL_INSTALL_PATH \
-    ninja -j8 && ninja install
-    popd
-elif [[ "$1" == "soc" ]]; then
-    mkdir -p $TMP_WORKING_DIR/build_sdk
-    pushd $TMP_WORKING_DIR/build_sdk
-    cmake -G Ninja $IVE_ROOT -DCVI_TARGET=soc \
-                             -DENABLE_SYSTRACE=OFF \
-                             -DCMAKE_BUILD_TYPE=SDKRelease \
-                             -DKERNEL_HEADERS_ROOT=$KERNEL_HEADER_PATH \
-                             -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                             -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                             -DCMAKE_INSTALL_PREFIX=$IVE_SDK_INSTALL_PATH \
-                             -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                             -DCMAKE_TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-aarch64-linux.cmake
-    ninja -j8 && ninja install
-    popd
-elif [[ "$1" == "soc32" ]]; then
-    mkdir -p $TMP_WORKING_DIR/build_sdk
-    pushd $TMP_WORKING_DIR/build_sdk
-    echo "TOOLCHAIN_FILE=$TOOLCHAIN_FILE"
-    if [[ "$SDK_VER" == "uclibc" ]]; then
-	TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-uclibc-linux.cmake
-    else
-	TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-gnueabihf-linux.cmake
-    fi
-    cmake -G Ninja $IVE_ROOT -DCVI_TARGET=soc \
-                             -DENABLE_SYSTRACE=OFF \
-                             -DCMAKE_BUILD_TYPE=SDKRelease \
-                             -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
-                             -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
-                             -DCMAKE_INSTALL_PREFIX=$IVE_SDK_INSTALL_PATH \
-                             -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
-                             -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
-    ninja -j8 && ninja install
-    popd
+
+mkdir -p $TMP_WORKING_DIR/build_sdk
+pushd $TMP_WORKING_DIR/build_sdk
+
+if [[ "$SDK_VER" == "uclibc" ]]; then
+    TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-uclibc-linux.cmake
+elif [[ "$SDK_VER" == "64bit" ]]; then
+    TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-aarch64-linux.cmake
+elif [[ "$SDK_VER" == "32bit" ]]; then
+    TOOLCHAIN_FILE=$IVE_ROOT/toolchain/toolchain-gnueabihf-linux.cmake
 else
-  echo "Unsupported build type."
-  exit 1
+    echo "Wrong SDK_VER=$SDK_VER"
+    exit 1
 fi
+
+cmake -G Ninja $IVE_ROOT -DCVI_TARGET=soc \
+                            -DENABLE_SYSTRACE=OFF \
+                            -DCMAKE_BUILD_TYPE=SDKRelease \
+                            -DKERNEL_HEADERS_ROOT=$KERNEL_HEADER_PATH \
+                            -DMLIR_SDK_ROOT=$TPU_SDK_INSTALL_PATH \
+                            -DMIDDLEWARE_SDK_ROOT=$MW_PATH \
+                            -DCMAKE_INSTALL_PREFIX=$IVE_SDK_INSTALL_PATH \
+                            -DTOOLCHAIN_ROOT_DIR=$HOST_TOOL_PATH \
+                            -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE
+ninja -j8 || exit 1
+ninja install || exit 1
+popd
+
 # cd $TMP_WORKING_DIR
 # echo "Compressing SDK release..."
 # tar cf - ivesdk -P | pv -s $(du -sb ivesdk | awk '{print $1}') | gzip > $IVE_ROOT/$IVE_SDK_NAME.tar.gz
